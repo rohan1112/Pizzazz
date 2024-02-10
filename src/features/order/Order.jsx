18ccd6e3f8a8958f75c17701d25c357c52744b4d
rohan-1112 <rohankalaskar1112@gmail.com>
2024-02-10 19:01:11 +0530

    Update Order component to fetch and display ingredients for each item in the order.

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,5 +1,6 @@
 // Test ID: IIDSAT
 
+import { useFetcher } from 'react-router-dom';
 import { useLoaderData } from 'react-router';
 import { getOrder } from '../../services/apiRestaurant';
 import {
@@ -8,6 +9,7 @@ import {
   formatDate,
 } from '../../utils/helpers';
 import OrderItem from './OrderItem';
+import { useEffect } from 'react';
 
 // const order = {
 //   id: "ABCDEF",
@@ -45,7 +47,16 @@ import OrderItem from './OrderItem';
 // };
 
 function Order() {
+  const fetcher = useFetcher();
   const order = useLoaderData();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+    },
+    [fetcher],
+  );
+  console.log(fetcher.data);
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -88,7 +99,15 @@ function Order() {
 
       <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -4,13 +4,16 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li className="px-6 py-3">
+    <li className="space-y-1 px-6 py-3">
       <div className="flex items-center justify-between gap-4 text-sm">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className=" text-sm capitalize italic text-slate-500">
+        {isLoadingIngredients ? 'loading...' : ingredients.join(',')}
+      </p>
     </li>
   );
 }
